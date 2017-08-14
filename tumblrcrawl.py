49c4336c075629a3907547bbd98d9549e8906bd6
Mark <magpie240@gmx.com>
2017-08-15 07:21:18 +1000

    Loop and get page offsets to API

--- tumblrcrawl.py
+++ tumblrcrawl.py
@@ -28,25 +28,44 @@ import datetime
 import datedelta
 import subprocess
 
+# Container to hold filenames
 FILELIST = []
 
+# Number of posts to retrieve on each call to Tumblr (default is 20, max is 50)
+NUMBER = 50
+
+def add_to_list(xmldata, beginning):
+    # Are we prior to our end date
+    flag = True
+    
+    # Reply now a dictionary. Look down into <Post> fot the date tag
+    for posts in xmldata['tumblr']['posts']['post']:
+        date_of_post = posts['@date-gmt'].split()[0]
+        
+        if date_of_post > beginning:
+            #print(beginning, date_of_post)
+            try:
+                #photo_num = len(posts['photoset']['photo'])
+                #for i in range(0, photo_num):
+                for sets in posts['photoset']['photo']:
+                    FILELIST.append(sets['photo-url'][0]['#text'])
+                    #FILELIST.append(posts['photoset']['photo'][i]['photo-url'][0]['#text'])
+            except:
+                FILELIST.append(posts['photo-url'][0]['#text'])
+        else:
+            flag = False
+    
+    return flag
 
 def generate_photo_list(NUMBER):
     backdate = 0
+    proceed = True
+    offset = 0
+    counter = 0
     
     # How far back (in months) to go. 0 is no limit.
     if len(sys.argv) == 3:
         backdate = int(sys.argv[2])
-    
-    # Get site info from Tumblr
-    tumblr_url = "http://{0}.tumblr.com/api/read?type=photo&num={1}&start=0"
-    site_url = tumblr_url.format(sys.argv[1], NUMBER)
-    
-    try:
-        response = urllib.request.urlopen(site_url)
-    except urllib.error.URLError as e:
-        sys.stderr.write(e.reason + '\n')
-        sys.exit(1)
     
     # Only crawl last number of months
     if backdate > 0:
@@ -56,22 +75,25 @@ def generate_photo_list(NUMBER):
     else:
         beginning = "2012-01-01"
     
-    data = xmltodict.parse(response.read())
-    
-    # Reply now a dictionary. Look down into <Post> fot the date tag
-    for posts in data['tumblr']['posts']['post']:
-        date_of_post = posts['@date-gmt'].split()[0]
+    while proceed:
+        counter += 1
+        print("Getting Page " + str(counter))
+        # Get site info from Tumblr
+        tumblr_url = "http://{0}.tumblr.com/api/read?type=photo&num={1}&start={2}"
+        site_url = tumblr_url.format(sys.argv[1], NUMBER, offset)
         
-        if date_of_post > beginning:
-            try:
-                photo_num = len(posts['photoset']['photo'])
-                for i in range(0, photo_num):
-                    FILELIST.append(posts['photoset']['photo'][i]['photo-url'][0]['#text'])
-            except:
-                FILELIST.append(posts['photo-url'][0]['#text'])
-        else:
-            pass
+        try:
+            response = urllib.request.urlopen(site_url)
+        except urllib.error.URLError as e:
+            sys.stderr.write(e.reason + '\n')
+            sys.exit(1)
+        
+        data = xmltodict.parse(response.read())
+        proceed = add_to_list(data, beginning)
+        offset += NUMBER
     
+    
+def start_aria_job():
     # Not interested in GIFs
     newlist = [ x for x in FILELIST if x.find(".gif") == -1]
     # Remove duplicates
@@ -83,7 +105,7 @@ def generate_photo_list(NUMBER):
             f.write(s + '\n')
     
     # Run aria2c to do the work
-    subprocess.call(["aria2c", "-j4", "-i", "manifest", "-c", "-d", sys.argv[1]])
+    subprocess.call(["aria2c", "-j4", "-i", "manifest", "-s1", "-c", "-d", sys.argv[1]])
     
     # Cleanup
     os.remove("manifest")
@@ -91,9 +113,10 @@ def generate_photo_list(NUMBER):
 
 if __name__ == "__main__":
     sites = None
-    NUMBER = 48
     
     if len(sys.argv) < 2:
         sys.stderr.write("I need a tumblr name to crawl\n")
     
     generate_photo_list(NUMBER);
+    print("Total posts " + str(len(FILELIST)))
+    start_aria_job()
